c4ca2c3d78569b4f8a264bea4f3e083492c84b3c
Andres Felipe Graciano Monsalve <72512285+afgraciano@users.noreply.github.com>
2024-09-04 18:58:56 -0500

    conectado por intellij

--- src/main/java/com/emazon/api_stock/application/mapper/CategoryMapper.java
+++ src/main/java/com/emazon/api_stock/application/mapper/CategoryMapper.java
@@ -4,14 +4,15 @@ import com.emazon.api_stock.domain.model.Category;
 import com.emazon.api_stock.web.dto.DtoCategoryRequest;
 import com.emazon.api_stock.web.dto.DtoCategoryResponse;
 import org.mapstruct.Mapper;
-
+import org.mapstruct.Mapping;
 
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "description", target = "description")
+    Category dtoCategoryToCategory(DtoCategoryRequest dtoCategoryRequest);
 
     DtoCategoryResponse categoryToDtoCategory(Category category);
-
-    Category dtoCategoryToCategory(DtoCategoryRequest dtoCategoryRequest);
 
 }
